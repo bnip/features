b008afed81c34fe8e293d14279a457244f2c5808
Bryan Nip <bryannip@buffalo.edu>
2020-03-17 00:39:51 -0400

    When to use type annotations (any)

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -6,3 +6,30 @@ let nothing: undefined = undefined;
 
 // built in objects
 let now: Date = new Date();
+
+// Array
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, true, false];
+
+// Class
+class Car {}
+
+let car: Car = new Car();
+
+// Object literal
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20
+};
+
+// Function
+const longNumber: (i: number) => void = (i: number) => {
+  console.log(i);
+};
+
+// When to use annotations
+// 1) Function that returns the 'any' type
+const json = '{"x": 10, "y": 20}';
+const coordinates: { x: number; y: number } = JSON.parse(json);
+console.log(coordinates); // {x:10 y:20};
