819d5106e55a7f14aa5e7d791de7e85bfd2570cd
Bryan Nip <bryannip@buffalo.edu>
2020-03-27 14:29:48 -0400

    Functions in Interfaces

--- interfaces.ts
+++ interfaces.ts
@@ -1,19 +1,33 @@
 interface Vehicle {
   name: string;
-  year: number;
+  year: Date;
   broken: boolean;
+  summary(): string;
+}
+
+interface Reportable {
+  summary(): string;
 }
 
 const oldCivic = {
   name: 'civic',
-  year: 2000,
-  broken: true
+  year: new Date(),
+  broken: true,
+  summary(): string {
+    return `Name: ${this.name}`;
+  }
 };
 
 const printVehicle = (vehicle: Vehicle): void => {
   console.log(`Name: ${vehicle.name}`);
   console.log(`Year: ${vehicle.year}`);
   console.log(`Broken: ${vehicle.broken}`);
+  console.log(vehicle.summary());
+};
+
+const printSummary = (item: Reportable): void => {\
+  console.log(item.summary());
 };
 
 printVehicle(oldCivic);
+printSummary(oldCivic);
